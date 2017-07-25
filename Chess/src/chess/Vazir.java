2fe32782da16d060aac31a595404354a86bbb5c5
Mahdiar <mahdiyar.oraei@gmail.com>
2017-07-25 11:56:13 +0430

    shah & vizir isValidMove

--- Chess/src/chess/Vazir.java
+++ Chess/src/chess/Vazir.java
@@ -9,13 +9,18 @@ package chess;
  *
  * @author mahdiar
  */
-public class Vazir extends Mohre
-{
+public class Vazir extends Mohre {
 
     @Override
     public boolean isValidMove(Position p) {
-        
-        return true;
+
+        if (Math.abs(p.getRow() - position.getRow()) == Math.abs(p.getCol() - position.getCol())) { // Check crosswise move
+            return true;
+        } else if (p.getRow() - position.getRow() == 0 || p.getCol() - position.getCol() == 0) { // Check horizontal and vertical move
+            return !(p.getRow() - position.getRow() == 0 && p.getCol() - position.getCol() == 0); // Check not current position
+        } else {
+            return false;
+        }
     }
-    
+
 }
