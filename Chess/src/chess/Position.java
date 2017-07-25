6cea07bddc958c0984995080f8faa474d8bf42d6
Mahdiar <mahdiyar.oraei@gmail.com>
2017-07-25 11:26:09 +0430

    Position class added

--- Chess/src/chess/Position.java
+++ Chess/src/chess/Position.java
@@ -10,5 +10,38 @@ package chess;
  * @author mahdiar
  */
 public class Position {
-    
+
+    public static final int ROW_COUNT = 8;
+    public static final int COL_COUNT = 8;
+    private int row;
+    private int col;
+
+    public Position(int row, int col) throws InvalidRowException, InvalidColException {
+        setRow(row);
+        setCol(col);
+    }
+
+    public void setRow(int row) throws InvalidRowException {
+        if (row > ROW_COUNT || row < 1) {
+            throw new InvalidRowException();
+        } else {
+            this.row = row;
+        }
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setCol(int col) throws InvalidColException{
+        if (col > COL_COUNT || col < 1) {
+            throw new InvalidColException();
+        } else {
+            this.col = col;
+        }
+    }
+
+    public int getCol() {
+        return col;
+    }
 }
