0485651ad70bfe2cf04f9f2d4abee43e1f6febd1
ALI\Ali_M <ali.mohammadzadeh.1376@gmail.com>
2017-07-25 12:29:31 +0430

    add getBlackPlayer and getWhitePlayer to player class

--- Chess/src/chess/Player.java
+++ Chess/src/chess/Player.java
@@ -10,5 +10,21 @@ package chess;
  * @author mahdiar
  */
 public class Player {
+    private static Player whitePlayer;
+    private static Player blackPlayer;
     
+    public static Player getWhitePlayer(){//check for white player
+        if(whitePlayer== null){
+            whitePlayer=new Player();
+            return whitePlayer;
+        }
+        else return whitePlayer;
+    }
+    public static Player getBlackPlayer(){//check for black player
+        if(blackPlayer== null){
+            blackPlayer=new Player();
+            return blackPlayer;
+        }
+        else return blackPlayer;
+    }
 }
